33e6cfa36ef48391285554e7a0cdf5a8a2c4938b
antagomir <leo.lahti@iki.fi>
2015-07-18 19:01:44 +0100

    ok

--- R/harmonize_names.R
+++ R/harmonize_names.R
@@ -57,9 +57,9 @@ harmonize_names <- function (x, synonymes, remove.unknown = FALSE, check.synonym
 
     xx <- xuniq
     for (i in 1:nrow(synonymes)) {
-      print(synonymes[i,])
+      #print(synonymes[i,])
       xx <- gsub(synonymes[i, "synonyme"], synonymes[i, "name"], xx)
-      print(xx)
+      #print(xx)
     }
   
     xx2 <- xx[match(xorig, xuniq)]
