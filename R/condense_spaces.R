3eaaf4e79160f0f8721b9c5cc375b5dcbe6e1b80
hegroiva <hege.roivainen@gmail.com>
2015-10-20 12:59:45 +0300

    Added condense_spaces
    Added condense_spaces

---
+++ R/condense_spaces.R
@@ -0,0 +1,25 @@
+#' @title condense_spaces
+#' @description Trim and remove double spaces from the input strings
+#'
+#' @param x A vector
+#' @return A vector with spaces removed
+#'
+#' @details Beginning, double and ending 
+#'          spaces are also removed from the strings.
+#'
+#' @export
+#' 
+#' @author Leo Lahti \email{leo.lahti@@iki.fi}
+#' @references See citation("sorvi")
+#' 
+#' @examples \dontrun{x2 <- condense_spaces(x)}
+#' @keywords utilities
+condense_spaces <- function (x) 
+{
+    x <- str_trim(x)
+    while (length(grep("  ", x)) > 0) {
+        x <- gsub("  ", " ", x)
+    }
+    x[x == ""] <- NA
+    x
+}
