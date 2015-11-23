2cf47f26672c8b5a4bdfd50682b351a648ca79ac
antagomir <leo.lahti@iki.fi>
2015-11-23 11:26:47 -0200

    checl synonymes update

--- R/synonymes.R
+++ R/synonymes.R
@@ -1,11 +1,12 @@
 #' @title check_synonymes
-#' @description Check synonyme table. 
+#' @description Check synonyme table. Remove ambiguous mappings.
 #' @param synonymes synonymes data.frame with the self-explanatory fields 'name' and 'synonyme'.
 #' @param include.lowercase Include lowercase versions of the synonymes
 #' @param verbose verbose
 #' @param sort Sort synonymes
 #' @param self Ensure that each name is synonyme for itself (this may
 #'             cause ambiguous mappings, use with care !)
+#' @param fill_empty If name field is empty, accept the given synonyme as the final name.
 #' @return Polished synonyme table
 #' @export
 #' @details Remove duplicated information. Ensure identical matches
@@ -14,12 +15,23 @@
 #' @references See citation("sorvi")
 #' @examples \dontrun{s <- check_synonymes(synonymes)}
 #' @keywords utilities
-check_synonymes <- function (synonymes, include.lowercase = TRUE, verbose = FALSE, sort = FALSE, self = FALSE) {
+check_synonymes <- function (synonymes, include.lowercase = TRUE, verbose = FALSE, sort = FALSE, self = FALSE, fill_empty = FALSE) {
 
   synonyme <- NULL
+  synonymes <- as.data.frame(synonymes, stringsAsFactors = FALSE)
 
   # Remove comment columns
   synonymes <- synonymes[, c("synonyme", "name")]
+  synonymes$name <- as.character(synonymes$name)
+  synonymes$synonyme <- as.character(synonymes$synonyme)    
+
+  # If name field is not given, accept the synonyme as the final name
+  if (fill_empty) {
+    inds <- which(synonymes$name == "")
+    if (length(inds) > 0) {
+      synonymes$name[inds] <- synonymes$synonyme[inds]
+    }
+  }
 
   # Ensure each proper name is synonyme also for itself
   if (self) {
@@ -27,20 +39,13 @@ check_synonymes <- function (synonymes, include.lowercase = TRUE, verbose = FALS
     	          synonyme = as.character(synonymes$synonyme))
     tmp2 <- cbind(name = as.character(synonymes$name),
     	          synonyme = as.character(synonymes$name))
-    synonymes <- rbind(tmp1, tmp2)
+    synonymes <- as.data.frame(rbind(tmp1, tmp2), stringsAsFactors = FALSE)
   }
-
-  # Remove duplicates
-  synonymes <- unique(synonymes)
-
-  # Make data frame
-  synonymes <- as.data.frame(synonymes, stringsAsFactors = FALSE)
 
   # Include lowercase versions of the synonymes
   if (include.lowercase) {
     if (verbose) { message("Including lowercase versions of the synonymes") }
     synonymes <- rbind(synonymes, cbind(name = as.character(synonymes$name), synonyme = tolower(as.character(synonymes$synonyme))))
-    synonymes <- unique(synonymes)    
   }
   
   # Remove duplicated info
