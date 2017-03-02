15150ee7fc1175f991fa25f93c1ff79fff35dcd7
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2017-03-02 12:46:37 +0100

    Use key:value URLs

--- R/ropengov_storage_path.R
+++ R/ropengov_storage_path.R
@@ -1,8 +1,8 @@
 
 #' ropengov_storage_path
 #'
-#' Arguments:
-#'   ... Arguments to pass
+#' @param x Characted string key defining which storage path
+#' is requested.
 #'
 #' Return:
 #' @return URL for Louhos data
@@ -14,14 +14,19 @@
 #' See citation("sorvi")
 #' @author Leo Lahti \email{louhos@@googlegroups.com}
 #' @keywords utilities
-ropengov_storage_path <- function() {
+ropengov_storage_path <- function(x) {
   # Louhos data is stored in Github avoindata repo:
-  # https://github.com/avoindata/ which is
-  # mirrored on Datavaalit server
-  #"http://www.datavaalit.fi/storage/avoindata/"
-
-  # Load data directly from GitHub
-  storage_path <- "https://github.com/avoindata/mml/blob/master/"
+  # https://github.com/avoindata/
+  storage_paths <- list(
+    "mml" = "https://github.com/avoindata/mml/blob/master/rdata/",
+    "louhos" = "https://github.com/avoindata/louhos/blob/master/"
+  )
+  if (x %in% names(storage_paths)) {
+    storage_path <- storage_paths[x]
+  } else {
+    stop("x is not a suitable key to stored paths: ",
+         paste(names(storage_paths), collapse = ", "))
+  }
   return(storage_path)
 }
 
