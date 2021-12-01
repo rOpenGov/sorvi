1e13b25ea2c4a6aaa2d13e9aa5396f106027cc85
Pyry Kantanen <pyry.kantanen@gmail.com>
2021-12-01 14:25:23 +0200

    Minor fixes, more comprehensive documentation

--- R/get_ifpi_charts.R
+++ R/get_ifpi_charts.R
@@ -8,13 +8,27 @@
 #' blog post "Beginner's Guide on Web Scraping in R" on analyticsvidhya.com. 
 #' Downloads chart data from Musiikkituottajat - IFPI Finland ry website.
 #' Please note that this function works only with IFPI Finland website!
+#' 
+#' The output has the following columns:
+#' \itemize{
+#'   \item{rank_data: Rank on chart}
+#'   \item{artist_data: Artist name}
+#'   \item{song_title_data: Song title}
+#'   \item{rank_last_week: Rank on chart on the previous week. RE if the song has
+#' re-entered the chart}
+#'   \item{chart_woc_data: Weeks on chart}
+#'   \item{week: Week number of observation}
+#'   \item{year: Year of observation}
+#' }
 #'
 #' @param channel Options: "radio", "albumit", "singlet", "fyysiset-albumit"
-#' @param year year as numeric, default is 2020
-#' @param week week as numeric, default is 50. Please note that number of weeks 
-#' differ between years. For simplicity's sake valid weeks are set to be 
-#' between 1 and 53. Use e.g. `lubridate::isoweek` to check how many weeks a
-#' given year has.
+#' @param year year as numeric. Default is NA, returning charts from current 
+#' year. Charts are available from 2014 onwards.
+#' @param week week as numeric. Default is NA, returning most last possible 
+#' charts. Week cannot be the current week.
+#' Please note that number of weeks differ between years. 
+#' For simplicity's sake valid weeks are set to be between 1 and 53. 
+#' Use e.g. `lubridate::isoweek` to check how many weeks a given year has.
 #' 
 #' @return tibble
 #'
@@ -22,18 +36,36 @@
 #' @importFrom rvest html_elements html_text
 #' @importFrom xml2 read_html as_list
 #' @importFrom assertthat assert_that is.number is.string
+#' @importFrom lubridate isoweek isoyear
 #' 
 #' @author Pyry Kantanen <pyry.kantanen@@gmail.com>
 #' 
 #' @seealso Original tutorial in \url{https://www.analyticsvidhya.com/blog/2017/03/beginners-guide-on-web-scraping-in-r-using-rvest-with-hands-on-knowledge/}
 #'
 #' @export
-get_ifpi_charts <- function(channel = "radio", year = 2020, week = 50){
+get_ifpi_charts <- function(channel = "radio", year = NA, week = NA){
   
-  assert_that(assertthat::is.number(year))
-  assert_that(year >= 2014)
-  assert_that(assertthat::is.number(week))
-  assert_that(week >= 1 && week <= 53)
+  if (!is.na(year)){
+    assert_that(assertthat::is.number(year))
+    assert_that(year >= 2014)
+    assert_that(year <= lubridate::isoyear(Sys.time()))
+  }
+  
+  if (is.na(year) && !is.na(week)){
+    # Set year as the current year
+    message("Year set to NA. Downloading charts for current year")
+    year <- lubridate::isoyear(Sys.time())
+  }
+  if (!is.na(year) && is.na(week)){
+    stop("Cannot return charts for whole year, please select a valid week")
+  }
+  
+  if (!is.na(week)){
+    assert_that(assertthat::is.number(week))
+    assert_that(week >= 1 && week <= 53)
+    assert_that(week < lubridate::isoweek(Sys.time()))
+  }
+  
   assert_that(assertthat::is.string(channel))
   assert_that(channel %in% c("radio", "albumit", "singlet", "fyysiset-albumit"))
   
@@ -42,6 +74,7 @@ get_ifpi_charts <- function(channel = "radio", year = 2020, week = 50){
   if (is.numeric(year) && is.numeric(week)){
     url <- paste(base_url, channel, year, week, sep = "/") 
   } else {
+    # most recent charts
     url <- paste(base_url, channel, sep = "/")
   }
   
@@ -69,19 +102,24 @@ get_ifpi_charts <- function(channel = "radio", year = 2020, week = 50){
   chart_change_data_html <- rvest::html_elements(webpage,'.chart-change')
   chart_change_list <- xml2::as_list(chart_change_data_html)
   
+  # Chart change node is a mess so this custom for-loop is needed
   rank_last_week <- rep(NA, length(chart_change_list))
   for (i in 1:length(chart_change_list)){
     if (length(chart_change_list[[i]]) == 3){
       if (attributes(chart_change_list[[i]]$div)$title == "Samalla sijalla"){
         last_position <- rank_data[i]
       } else if (attributes(chart_change_list[[i]]$div)$title == "Paluu listalle"){
-        last_position <- NA
+        last_position <- "RE"
+      } else if (attributes(chart_change_list[[i]]$div)$title == "Uusi"){
+        last_position <- "UUSI"
       }
     } else if (length(chart_change_list[[i]]) == 5){
       if (attributes(chart_change_list[[i]]$div)$title == "Nousussa"){
         last_position <- as.numeric(chart_change_list[[i]][4]$div[[1]])
       } else if (attributes(chart_change_list[[i]]$div)$title == "Viime viikolla"){
         last_position <- as.numeric(chart_change_list[[i]][2]$div[[1]])
+      } else {
+        last_position <- NA
       }
     } else {
       last_position <- NA
@@ -92,6 +130,13 @@ get_ifpi_charts <- function(channel = "radio", year = 2020, week = 50){
   # Weeks on chart
   chart_woc_data_html <- rvest::html_elements(webpage,'.chart-woc')
   chart_woc_data  <-  as.numeric(rvest::html_text(chart_woc_data_html))
+  
+  if (is.na(year) || is.na(week)){
+    week_year_html <- rvest::html_elements(webpage,'.selector-toggle')
+    week_year <- rvest::html_text(week_year_html)
+    week <- regmatches(week_year, regexpr("[0-9]+(?=\\/)", week_year, perl = TRUE))
+    year <- regmatches(week_year, regexpr("(?<=\\/)[0-9]+", week_year, perl = TRUE))
+  }
   
   final_output <- dplyr::tibble(rank_data, artist_data, song_title_data, rank_last_week, chart_woc_data, week, year)
   final_output
