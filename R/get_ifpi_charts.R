53e82bf7f40f1306195811161c63b055c0771cc0
Pyry Kantanen <pyry.kantanen@gmail.com>
2021-12-01 16:50:38 +0200

    Rename column names, use checkmate instead of assertthat

--- R/get_ifpi_charts.R
+++ R/get_ifpi_charts.R
@@ -11,12 +11,12 @@
 #' 
 #' The output has the following columns:
 #' \itemize{
-#'   \item{rank_data: Rank on chart}
-#'   \item{artist_data: Artist name}
-#'   \item{song_title_data: Song title}
+#'   \item{rank: Rank on chart}
+#'   \item{artist: Artist name}
+#'   \item{song_title: Song title}
 #'   \item{rank_last_week: Rank on chart on the previous week. RE if the song has
 #' re-entered the chart}
-#'   \item{chart_woc_data: Weeks on chart}
+#'   \item{chart_woc: Weeks on chart}
 #'   \item{week: Week number of observation}
 #'   \item{year: Year of observation}
 #' }
@@ -35,8 +35,8 @@
 #' @importFrom dplyr tibble
 #' @importFrom rvest html_elements html_text
 #' @importFrom xml2 read_html as_list
-#' @importFrom assertthat assert_that is.number is.string
-#' @importFrom lubridate isoweek isoyear
+#' @importFrom checkmate assert_number assert_string assert_choice
+#' @importFrom lubridate isoyear
 #' 
 #' @author Pyry Kantanen <pyry.kantanen@@gmail.com>
 #' 
@@ -45,58 +45,54 @@
 #' @export
 get_ifpi_charts <- function(channel = "radio", year = NA, week = NA){
   
-  if (!is.na(year)){
-    assert_that(assertthat::is.number(year))
-    assert_that(year >= 2014)
-    assert_that(year <= lubridate::isoyear(Sys.time()))
-  }
+  assert_number(year, na.ok = TRUE, lower = 2014, upper = lubridate::isoyear(Sys.time()))
+  assert_number(week, na.ok = TRUE, lower = 1, upper = 53)
+  
+  assert_string(channel, na.ok = FALSE)
+  
+  assert_choice(channel,
+                choices = c("radio", "albumit", "singlet", "fyysiset-albumit"),
+                null.ok = FALSE)
   
   if (is.na(year) && !is.na(week)){
     # Set year as the current year
     message("Year set to NA. Downloading charts for current year")
     year <- lubridate::isoyear(Sys.time())
   }
+
   if (!is.na(year) && is.na(week)){
     stop("Cannot return charts for whole year, please select a valid week")
   }
-  
-  if (!is.na(week)){
-    assert_that(assertthat::is.number(week))
-    assert_that(week >= 1 && week <= 53)
-    assert_that(week < lubridate::isoweek(Sys.time()))
-  }
-  
-  assert_that(assertthat::is.string(channel))
-  assert_that(channel %in% c("radio", "albumit", "singlet", "fyysiset-albumit"))
-  
+
   base_url <- "https://www.ifpi.fi/lista"
-  
-  if (is.numeric(year) && is.numeric(week)){
+
+  if (!is.na(year) && !is.na(week)){
     url <- paste(base_url, channel, year, week, sep = "/") 
   } else {
     # most recent charts
     url <- paste(base_url, channel, sep = "/")
   }
   
-  #Reading the HTML code from the website
-  webpage  <-  xml2::read_html(url)
+  # General example
+  # Read the HTML code from the website
+  webpage <- xml2::read_html(url)
   
-  # sTEP 2: Using CSS selectors to scrap the rankings section
-  rank_data_html  <-  rvest::html_elements(webpage,'.chart-position')
+  # Use CSS selectors to scrape chart positions for chosen week
+  rank_data_html <- rvest::html_elements(webpage,'.chart-position')
   
-  # Step 3; Converting the ranking data to text
-  rank_data  <-  rvest::html_text(rank_data_html)
+  # Convert raw html to text
+  rank_data <- rvest::html_text(rank_data_html)
   
-  # Step 4: Data-Preprocessing: Converting rankings to numerical
-  rank_data <- as.numeric(rank_data)
+  # Convert rank_data strings to numerical
+  rank <- as.numeric(rank_data)
   
   # Artist name
   artist_data_html <- rvest::html_elements(webpage,'.chart-artist')
-  artist_data  <-  rvest::html_text(artist_data_html)
+  artist <- rvest::html_text(artist_data_html)
   
   # Song title
-  song_title_data_html  <-  rvest::html_elements(webpage,'.chart-title')
-  song_title_data  <-  rvest::html_text(song_title_data_html)
+  song_title_data_html <- rvest::html_elements(webpage,'.chart-title')
+  song_title <- rvest::html_text(song_title_data_html)
   
   # Position on chart last week
   chart_change_data_html <- rvest::html_elements(webpage,'.chart-change')
@@ -107,7 +103,7 @@ get_ifpi_charts <- function(channel = "radio", year = NA, week = NA){
   for (i in 1:length(chart_change_list)){
     if (length(chart_change_list[[i]]) == 3){
       if (attributes(chart_change_list[[i]]$div)$title == "Samalla sijalla"){
-        last_position <- rank_data[i]
+        last_position <- rank[i]
       } else if (attributes(chart_change_list[[i]]$div)$title == "Paluu listalle"){
         last_position <- "RE"
       } else if (attributes(chart_change_list[[i]]$div)$title == "Uusi"){
@@ -129,7 +125,7 @@ get_ifpi_charts <- function(channel = "radio", year = NA, week = NA){
   
   # Weeks on chart
   chart_woc_data_html <- rvest::html_elements(webpage,'.chart-woc')
-  chart_woc_data  <-  as.numeric(rvest::html_text(chart_woc_data_html))
+  chart_woc <- as.numeric(rvest::html_text(chart_woc_data_html))
   
   if (is.na(year) || is.na(week)){
     week_year_html <- rvest::html_elements(webpage,'.selector-toggle')
@@ -138,6 +134,6 @@ get_ifpi_charts <- function(channel = "radio", year = NA, week = NA){
     year <- regmatches(week_year, regexpr("(?<=\\/)[0-9]+", week_year, perl = TRUE))
   }
   
-  final_output <- dplyr::tibble(rank_data, artist_data, song_title_data, rank_last_week, chart_woc_data, week, year)
+  final_output <- dplyr::tibble(rank, artist, song_title, rank_last_week, chart_woc, week, year)
   final_output
 }
