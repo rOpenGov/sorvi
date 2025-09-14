f98b32d5bbd1ddbecfd7ab9bf15170e34cb16b32
Pyry Kantanen <pyry.kantanen@gmail.com>
2025-09-15 00:02:19 +0300

    Suppress NOTE

--- R/firstlib.R
+++ R/firstlib.R
@@ -1,12 +1,15 @@
 # When one has weighed the sun in the balance, and measured the
 # steps of the moon, and mapped out the seven heavens star by star,
 # there still remains oneself. Who can calculate the orbit of his own
-# soul? - Oscar Wilde 
+# soul? - Oscar Wilde
 
 .onAttach <- function(lib, pkg)
 {
 
-  packageStartupMessage("sorvi - Tools for Finnish Open Data.\nCopyright (C) 2010-2023 Leo Lahti, Juuso Parkkinen, Joona Lehtomaki and Pyry Kantanen \n\nhttp://github.com/ropengov/sorvi \n\n Hard sciences are successful because they deal with soft problems; \n soft sciences are struggling because they deal with hard problems.\n-                        Von Foerster\n")
+  packageStartupMessage("sorvi - Tools for Finnish Open Data.\nCopyright (C) 2010-2025 Leo Lahti, Juuso Parkkinen, Joona Lehtomaki and Pyry Kantanen \n\nhttp://github.com/ropengov/sorvi \n\n Hard sciences are successful because they deal with soft problems; \n soft sciences are struggling because they deal with hard problems.\n-                        Von Foerster\n")
+
+  # dummy to suppress "All declared Imports should be used" NOTE
+  class <- R6::R6Class()
 
 }
 
