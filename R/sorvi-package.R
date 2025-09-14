3d7df924530500ba9689548d7a8516f3d899b805
Pyry Kantanen <pyry.kantanen@gmail.com>
2025-09-15 00:02:00 +0300

    Use new convention

--- R/sorvi-package.R
+++ R/sorvi-package.R
@@ -1,12 +1,11 @@
-#' The sorvi package hosts various functions that are mainly helpful in rOpenGov 
-#' package maintenance, package authoring and drawing graphs for presentations. 
+#' The sorvi package hosts various functions that are mainly helpful in rOpenGov
+#' package maintenance, package authoring and drawing graphs for presentations.
 #' Additionally it has some functions that do not (yet) have their own package
 #' but are useful in some contexts.
 #'
 #' @name sorvi-package
 #' @aliases sorvi
-#' @docType package
-#' @title Algorithmic Tools for Open Data in Finland 
+#' @title Algorithmic Tools for Open Data in Finland
 #'
 #' @author Leo Lahti, Juuso Parkkinen, Jussi Paananen, Joona Lehtomaki, Einari Happonen, Juuso Haapanen, and Pyry Kantanen \email{louhos@@googlegroups.com}
 #' @references
@@ -14,4 +13,4 @@
 #' \url{https://github.com/rOpenGov/sorvi}
 #' @examples library(sorvi)
 #' @keywords package
-NULL
+"_PACKAGE"
